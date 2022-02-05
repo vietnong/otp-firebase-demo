27607acf1bb2d43c482328d723b12f94ee8b1f26
hoangdoan-ntq <hoang.doan@ntq-solution.com.vn>
2022-02-05 22:52:11 +0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import firebase from './firebase';
+import { getAuth, signInWithPhoneNumber, RecaptchaVerifier } from "firebase/auth";
 import './App.css';
 
 function App() {
+  const [phoneNumber, setPhoneNumber] = useState();
+  const [otpNumber, setOtpNumber] = useState();
+  const configureReCaptcha = () => {
+    const auth = getAuth();
+    window.recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
+      'size': 'invisible',
+      'callback': (response) => {
+        // reCAPTCHA solved, allow signInWithPhoneNumber.
+        onSignInSubmit();
+      },
+      defaultCountry: 'VI'
+    }, auth);
+
+  }
+  const onSignInSubmit = (e) => {
+    e.preventDefault();
+    configureReCaptcha();
+    // const phoneNumber = getPhoneNumberFromUserInput();
+    const appVerifier = window.recaptchaVerifier;
+
+    const auth = getAuth();
+    signInWithPhoneNumber(auth, '+84' + phoneNumber, appVerifier)
+      .then((confirmationResult) => {
+
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+        window.confirmationResult = confirmationResult;
+        alert('Phone number is valid. Please wait and enter OTP')
+        // ...
+      }).catch((error) => {
+        alert('Phone number is invalid. Please try again')
+        // Error; SMS not sent
+        // ...
+      });
+  }
+  const otpSubmit = (e) => {
+    e.preventDefault();
+    window.confirmationResult.confirm(otpNumber).then((result) => {
+      // User signed in successfully.
+      const user = result.user;
+      alert('OTP is valid. Aihi')
+      // ...
+    }).catch((error) => {
+      alert('OTP is invalid. Ahuhu')
+      // User couldn't sign in (bad verification code?)
+      // ...
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Trang hâm hấp</h1>
+      <h1>Login Form</h1>
+      <form onSubmit={e => onSignInSubmit(e)}>
+        <div id="sign-in-button"></div>
+        <input type="number" value={phoneNumber} name="mobile" placeholder="Mobile Number" required onChange={e => setPhoneNumber(e.target.value)}></input>
+        <button type="submit">Submit</button>
+      </form>
+      <h1>Enter OTP</h1>
+      <form onSubmit={e => otpSubmit(e)}>
+        <input type="number" value={otpNumber} name="otp" placeholder="OTP Number" required onChange={e => setOtpNumber(e.target.value)}></input>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBcgU-_yvd_dw7xULnA-2968GbM543-Lmg",
+    authDomain: "otp-firebase-demo-ae794.firebaseapp.com",
+    projectId: "otp-firebase-demo-ae794",
+    storageBucket: "otp-firebase-demo-ae794.appspot.com",
+    messagingSenderId: "543749919268",
+    appId: "1:543749919268:web:527392c0cf6cf517b87eb1"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+export default firebase;
